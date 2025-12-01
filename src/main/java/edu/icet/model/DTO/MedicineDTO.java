9a2745f50a19508a06cdba5b28e65ee5ba56e0ad
SarangaKalhari <kalharisaranga9@gmail.com>
2025-12-01 11:13:00 +0530

    Created MedicineDTO

---
+++ src/main/java/edu/icet/model/DTO/MedicineDTO.java
@@ -0,0 +1,25 @@
+package edu.icet.model.DTO;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class MedicineDTO {
+
+    private String name;
+    private String brand;
+    private String supplierID;
+    private String batchNo;
+    private int quantity;
+    private BigDecimal cost;
+    private BigDecimal unitPrice;
+    private Date expiryDate;
+    private String category;
+
+}
