a112c781d6a80959b96b865eeb7d2c2a51f22257
SarangaKalhari <kalharisaranga9@gmail.com>
2025-12-02 15:17:20 +0530

    Implement Medicine UI and AdminDashboardForm features

--- src/main/java/edu/icet/controller/dashboards/AdminDashboardForm.java
+++ src/main/java/edu/icet/controller/dashboards/AdminDashboardForm.java
@@ -1,23 +1,58 @@
 package edu.icet.controller.dashboards;
 
+import edu.icet.controller.LoginFormController;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AdminDashboardForm {
+
+    Stage stage = new Stage();
 
     @FXML
     void dashboardOnAction(ActionEvent event) {
 
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/AdminDashboard.fxml"))));
+
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            currentStage.close();
+
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void loginOnAction(ActionEvent event) {
 
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/LoginForm.fxml"))));
+
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            currentStage.close();
+
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void medicineOnAction(ActionEvent event) {
 
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/MedicineManagement.fxml"))));
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
