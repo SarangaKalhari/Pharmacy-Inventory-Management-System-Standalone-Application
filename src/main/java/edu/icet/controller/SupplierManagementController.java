da4454ca9a4722c5164e451557204d1986cdf1b9
SarangaKalhari <kalharisaranga9@gmail.com>
2025-12-04 23:52:31 +0530

    Create Supplier Management UI

---
+++ src/main/java/edu/icet/controller/SupplierManagementController.java
@@ -0,0 +1,20 @@
+package edu.icet.controller;
+
+import javafx.event.ActionEvent;
+
+public class SupplierManagementController {
+    public void addOnAction(ActionEvent actionEvent) {
+    }
+
+    public void viewOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void reloadOnAction(ActionEvent actionEvent) {
+    }
+}
