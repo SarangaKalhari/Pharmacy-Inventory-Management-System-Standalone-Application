81b51b6bd4c9b5a94d019f2b6f20cd9ec8f66c8b
SarangaKalhari <kalharisaranga9@gmail.com>
2026-01-27 23:58:20 +0530

    Implement Cashier Dashboard UI for pharmacist role

--- src/main/java/edu/icet/controller/dashboards/CashierBillingDashboard.java
+++ src/main/java/edu/icet/controller/dashboards/CashierBillingDashboard.java
@@ -1,4 +1,33 @@
 package edu.icet.controller.dashboards;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class CashierBillingDashboard {
+
+    @FXML
+    void dashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loginOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void reportsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void salesOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void salesReportsOnAction(ActionEvent event) {
+
+    }
+
 }
