d4d4ef88f24dee6be86ac361ed00278d321a564f
SarangaKalhari <kalharisaranga9@gmail.com>
2026-01-28 00:15:57 +0530

    Implement Cashier Dashboard UI funtionalities for pharmacist role

--- src/main/java/edu/icet/controller/dashboards/CashierBillingDashboard.java
+++ src/main/java/edu/icet/controller/dashboards/CashierBillingDashboard.java
@@ -2,31 +2,85 @@ package edu.icet.controller.dashboards;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class CashierBillingDashboard {
+
+    Stage stage = new Stage();
 
     @FXML
     void dashboardOnAction(ActionEvent event) {
 
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/CashierBillingDashboard.fxml"))));
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            currentStage.close();
+
+            stage.setTitle("Pharmacist Dashboard");
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @FXML
     void loginOnAction(ActionEvent event) {
+
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/LoginForm.fxml"))));
+
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            currentStage.close();
+
+            stage.setTitle("Login");
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
     @FXML
     void reportsOnAction(ActionEvent event) {
 
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Report.fxml"))));
+            stage.setTitle("Inventory Report");
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @FXML
     void salesOnAction(ActionEvent event) {
 
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Sales.fxml"))));
+            stage.setTitle("Selling");
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 
     @FXML
     void salesReportsOnAction(ActionEvent event) {
+
+        try {
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/SalesReport.fxml"))));
+            stage.setTitle("Sales Reports");
+            stage.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
